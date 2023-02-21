1d873d7784fa8368a220b9230273a0a1e9b54f37
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-21 04:49:56 -0800

    add print_alphabet_x10 function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char alp;
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
 		_putchar('\n');
 	}
 }
