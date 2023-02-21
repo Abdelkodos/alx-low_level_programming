ccf32e1e1b70d6c3348ea03c86ac4da16a9a67bb
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-21 14:03:18 -0800

    fix the 104 file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,21 +6,27 @@
  */
 int main(void)
 {
-	int counter = 2;
+	unsigned long int i, j = 1, k = 2, l, a, b, c;
 
-	float o = 1, m = o + 1, n = o + m;
-
-	printf("%.0f, ", o);
-	printf("%.0f, ", m);
-	while (counter < 98)
+	printf("%lu", j);
+	for (i = 1; i < 91; i++)
 	{
-		counter++;
-		printf("%.0f", n);
-		o = m;
-		m = n;
-		n = o + m;
-		if (counter < 98)
-			printf(", ");
+		printf(", %lu", k);
+		k += j;
+		j = k - j;
+	}
+	l = j / 1000000000;
+	a = j % 1000000000;
+	b = k / 1000000000;
+	c = k % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", b + (c / 1000000000));
+		printf("%lu", c % 1000000000);
+		b += l;
+		l = b - l;
+		c += a;
+		a = c - a;
 	}
 	printf("\n");
 	return (0);
