04ea5ed118fcac5bbb73e7087d7d38416dcb8207
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-21 05:08:11 -0800

    add the _islower() function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,4 +20,12 @@ void print_alphabet(void);
  */
 
 void print_alphabet_x10(void);
+
+/**
+ * _islower(c) - pritn if character a lowercase alphabet
+ * @c: character to use
+ * Return 0 or 1
+ */
+
+int _islower(int c);
 #endif
