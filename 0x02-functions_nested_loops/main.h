b5c2c046657523f513d9b0910d65bda4038c318a
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-21 10:24:41 -0800

    add the _abs() function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -44,4 +44,41 @@ int _isalpha(int c);
  */
 
 int print_sign(int n);
+
+/**
+ * _abs - 
+ */
+
+int _abs(int);
+
+/**
+ * print_last_digit -
+ */
+
+int print_last_digit(int);
+
+/**
+ *
+ */
+
+void jack_bauer(void);
+
+/**
+ *
+ */
+
+void times_table(void);
+
+/**
+ *
+ */
+
+int add(int, int);
+
+/**
+ *
+ */
+
+void print_to_98(int n);
+
 #endif
