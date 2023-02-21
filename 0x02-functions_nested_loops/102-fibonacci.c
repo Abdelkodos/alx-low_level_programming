58bed2c0238d0a20ee4de0531a1a5f689c53059c
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-21 12:54:47 -0800

    add the 102-fibonacci file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int counter, limit = 50;
+	long a = 1, b = 2;
+
+	for (counter = 1; counter <= (limit / 2); counter++)
+	{
+		printf("%li %li ", a, b);
+		a += b;
+		b += a;
+	}
+	if (limit % 2 == 1)
+		printf("%li", a);
+
+	printf("\n");
+	return (0);
+}
