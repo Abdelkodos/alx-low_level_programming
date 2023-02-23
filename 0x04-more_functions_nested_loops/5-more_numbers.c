5129aa6832dcdf8d75c964e7f206a2105fcc3e99
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-23 05:27:17 -0800

    add 5th file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print ten times numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{	
+		j = 0;
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
