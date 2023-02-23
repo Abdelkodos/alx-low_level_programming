f4557923490b2106570a94b1c2e682a1b0a3e650
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-23 05:52:42 -0800

    add 6th file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int i;
+	
+	i = 0;
+
+	if (n > 0)
+	{
+		while (i <= (n - 1))
+		{
+			_putchar('_');
+			i++
+		}
+		_putchar('\n');
+	} else
+		_putchar('\n');
+}
