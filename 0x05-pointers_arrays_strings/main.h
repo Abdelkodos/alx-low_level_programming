579f7f4fdac53419557c4f77df8e9526a5b78ed0
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-02-27 10:23:01 -0800

    fix the header of the file file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: Abdelkouddouss Mekkaoui
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+void reset_to_98(int *n);
+
+#endif
