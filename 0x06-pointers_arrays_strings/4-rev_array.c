d4a2e02db5dffdd1e5c59601bfc0a8f526998688
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-03-01 13:20:13 -0800

    fix the task 4 solution

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,14 +1,14 @@
 /**
  * reverse_array - reverse the content of an array of integers
+ *
  * @a: array to reverse
  * @n: number of elements of array
- *
  */
 void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		j = a[i];
 		a[i] = a[n -i - 1];
