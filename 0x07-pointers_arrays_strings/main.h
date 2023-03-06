a21abe1f43e43a438c57c21fd175c1c24a4a6727
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-03-06 12:59:43 -0800

    fix the solution

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: main.h
- * Author: Abdelkouddouss Mekkaoui
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
- */
+#ifndef _MAIN_H
+#define _MAIN_H
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
