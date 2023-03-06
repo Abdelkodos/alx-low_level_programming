348e81ff103d4bdb464026172e1c033a891085fc
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-03-06 13:22:31 -0800

    add the solution of task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the chracter to be located
+ *
+ * Return: pointer to char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return s;
+	}
+	return NULL;
+}
