132dad91aea018af796db716dd5cbc4ec63a3ef8
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-03-13 05:21:16 -0700

    add the task 0 solution, create array of chars and initialize it

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Mekkaoui Abdelkouddouss
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-malloc_free directory.
+ */
+
+char *create_array(unsigned int size, char c);
+
+#endif
