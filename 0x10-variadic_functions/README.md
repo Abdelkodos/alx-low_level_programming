e41bb8ebe06ff52f29a6b4d824ff547ad1be6eb9
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-03-23 09:51:36 -0700

    add variadic functions project's folder and the solution of the first task

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+C Variadic functions project, learnt how to use `va_start`, `va_arg` and `va_end`
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/*
+ * File: variadic_functions.h
+ * Auth: Abdelkouddouss Mekkaoui
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x10-variadic_functions directory.
+ */
+
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
