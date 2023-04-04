4a6d794cd46884ec28579fdf5212026f4abb0192
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-03 23:29:49 -0700

    fix solution of task 1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,6 +8,7 @@
  * 	and types written in the 0x13-more_singly_linked_lists directory.
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -24,6 +25,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 
