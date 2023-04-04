296959f6a7a91344cc03473566dc1b0fa463506f
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-04 01:40:50 -0700

    add solution of task 10, delete a specific node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at a given index of a listint_t list.
- * @head: A pointer to the address of the head of the listint_t list.
- * @index: The index of the node to be deleted - indices start at 0.
+ * delete_nodeint_at_index - deletes the node at a given index of a listint_t list
+ * @head: a pointer to the address of the head of the listint_t list
+ * @index: the index of the node to be deleted - indices start at 0
  *
- * Return: On success - 1, On failure -1.
+ * return: on success - 1, on failure -1
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
