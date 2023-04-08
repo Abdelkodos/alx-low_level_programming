d7d681119dd5b9f7b35e7fadcf3f4fd318bbfdc5
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-08 09:12:56 -0700

    check 2, task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: string that contain the must convert number
+ *
  * Return: converted number or 0
  */
 
