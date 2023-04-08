cb586211f4ce223bc19705ebef64af1c2aff5167
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-08 08:46:47 -0700

    add first task solution, convert binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* binary_to_uint - converts a binary number to an unsigned int
+ * @b: string that contain the must convert number
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int length = 0, i;
+	unsigned int result = 0;
+
+	if (b == '\0')
+		return (0);
+
+	while (b[i] != '\0')
+		length++;
+	length--;
+
+	for (i = 0; i < length; i++)
+	{
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (result);
+
+		if (b[i] == '1')
+			result += (1 * (1 << length));
+
+		length--;
+	}
+
+	return (result);
+}
