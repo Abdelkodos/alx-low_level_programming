98b40ea4935a5dd6a438b48c4ce86fe57b55516c
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-09 18:59:04 -0700

    add solution of task 4, clearing a bit by set it to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - set the value of a bit at a given index to 0
+ * @n: integer to pass
+ * @index: index to set the value at
+ *
+ * return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+
+	(*n) &= ~(1 << index);
+
+	return (1);
+}
