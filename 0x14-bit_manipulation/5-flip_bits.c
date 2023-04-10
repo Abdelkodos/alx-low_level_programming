a8b2487489f203d343a9c453dbe48ffb1069d160
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-09 19:28:59 -0700

    add solution of task 5, count difference of fliped bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - returns the difference number of bits that we need to flip to get from n to m
+ * @n: the first number
+ * @m: number to flip n to
+ *
+ * Return: difference number of bits 
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flips = n ^ m, diff = 0;
+
+	while (flips > 0)
+	{
+		diff += (flips & 1);
+		flips >>= 1;
+	}
+
+	return (diff);
+}
