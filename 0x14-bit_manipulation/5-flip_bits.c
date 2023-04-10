a31bf4767683c3f24c99797637be3ff32754bd62
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-09 19:51:24 -0700

    fix betty style of task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,7 +8,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int flips = (n ^ m);
+	unsigned long int flips = n ^ m;
 	unsigned int diff = 0;
 
 	while (flips > 0)
