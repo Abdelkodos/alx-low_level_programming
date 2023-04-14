6d5e023f49e3c853a78d0f634d6efd4c02c3ba1b
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-14 09:10:35 -0700

    try to fix solution of task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * close_fl - closes file source file or reciever file.
+ * @fd: the file descriptor to be closed.
+ */
+void close_fl(int fd)
+{
+	int ch;
+
+	ch = close(fd);
+
+	if (ch == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - copies the contents of a file to another file.
  * @argc: number of arguments supplied to the program.
  * @argv: array of pointers to the arguments
@@ -17,7 +34,13 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buff = create_buff(argv[2]);
+	buff = malloc(sizeof(char) * 1024);
+	if (buff == NULL)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+                exit(99);
+        }
+
 	src = open(argv[1], O_RDONLY);
 	re = read(src, buff, 1024);
 	resv = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
