76c7bc0bb67143d5ee10054dc65c9ec951b81057
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-05-08 22:49:49 +0100

    add the file i/o project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads and prints the content of a text file
+ * @filename: the name of txt file
+ * @letters: the number of letters should be read and printed
+ * Return: printed letters count
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, re, wr;
+	char *buff;
+
+	buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL || filename == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	re = read(op, buff, letters);
+	wr = write(STDOUT_FILENO, buff, re);
+
+	if (op == (-1) || re == (-1) || wr == (-1) || wr != re)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(op);
+
+	return (wr);
+}
